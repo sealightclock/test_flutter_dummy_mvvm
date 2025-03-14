603e8ee8bb5deb17f55ef4b3be98cab1c5de30fa
Sealight Clock <chbebram@hotmail.com>
2025-03-14 11:24:00 -0700

    3rd commit: MyHomeScreen: Make it scrollable so that it works well in landscape mode.

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -11,33 +11,35 @@ class MyHomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text('MVVM with SharedPreferences')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Stored Value:", style: TextStyle(fontSize: 18)),
-            SizedBox(height: 10),
-            Text(
-              viewModel.myString.value,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter new value',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Stored Value:", style: TextStyle(fontSize: 18)),
+              SizedBox(height: 10),
+              Text(
+                viewModel.myString.value,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                viewModel.updateString(_controller.text);
-              },
-              child: Text('Update Value'),
-            ),
-          ],
+              SizedBox(height: 20),
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter new value',
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  viewModel.updateString(_controller.text);
+                },
+                child: Text('Update Value'),
+              ),
+            ],
+          ),
         ),
       ),
     );
